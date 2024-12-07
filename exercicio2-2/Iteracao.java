d472e23252b29744ce42fe2fe768464168a45b6f
Antônio Alexandre L. de Carvalho <aalcarvalho@celepar.pr.gov.br>
2024-12-07 04:03:58 -0300

    Exercicio 2.2

---
+++ exercicio2-2/Iteracao.java
@@ -0,0 +1,21 @@
+package iteracao;
+import java.util.List;
+import java.util.ArrayList;
+public class Iteracao{
+	public static void main(String[] args){
+		//float fishL[] = {1.9f, 2.8f, 13.1f, 29.2f, 35.3f};
+		//fishL=[1.9];
+		List<Float> fishL = new ArrayList<Float>();
+		fishL.add(1.9f);
+		fishL.add(13.1f);
+		fishL.add(29.5f);
+
+		Float tamReq = 28.f;
+
+		for(Float tamanho : fishL){
+			if(tamanho > tamReq){
+				System.out.println("Tamanho: "+tamanho);
+			}
+		}
+	}
+}
